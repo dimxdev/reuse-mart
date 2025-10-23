a81ae667234666cafbb3c49b01aaa29265a08efc
Dimas Brotowali Hidayatullah <brotowalidimas@gmail.com>
2025-10-23 23:29:01 +0700

    tambahin setup frontend

---
+++ frontend/Template.jsx
@@ -0,0 +1,10 @@
+function Template() {
+    const nama = "Dimas"
+    return (
+        <div>
+            <h1>namaku {nama}</h1>
+        </div>
+    )
+}
+
+export default Template
---
+++ frontend/src/pages/Testing.jsx
@@ -0,0 +1,11 @@
+import Button from "../components/atom/Button"
+
+function Testing() {
+    return (
+        <div>
+            <Button namaTombol="click me"/>
+        </div>
+    )
+}
+
+export default Testing
