26e243fb71d62355a995f7f204c58d4f84b94e81
Dimas Brotowali Hidayatullah <brotowalidimas@gmail.com>
2025-10-29 00:46:40 +0700

    nambah folder api & lib

--- frontend/src/components/atom/ButtonExample.jsx
+++ frontend/src/components/atom/ButtonExample.jsx
@@ -5,6 +5,7 @@ function ButtonExample(props) {
     return (
         <div>
             <h1 onClick={handleClick} className="bg-blue-400 text-center w-25 px-4 py-2 rounded-md text-white cursor-pointer hover:bg-blue-700">{namaTombol}</h1>
+
         </div>
     )
 }
