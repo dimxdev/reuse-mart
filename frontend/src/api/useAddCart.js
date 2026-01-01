c3001da3d06c3ae2b0bf4db70de58fcfef1afb07
Dimas Brotowali Hidayatullah <brotowalidimas@gmail.com>
2026-01-01 22:09:28 +0700

    nampilin jumlah cart di navbar

--- frontend/src/api/useAddCart.js
+++ frontend/src/api/useAddCart.js
@@ -1,19 +1,22 @@
+import { useWindow } from "../context/WindowContext";
 import axiosInstance from "../lib/axios";
 
 function useAddCart() {
+  const { handleRefreshWindow } = useWindow();
+
   const handleAddCart = async (productId) => {
     try {
       await axiosInstance.post("/cart", {
         productId: productId,
       });
-      console.log("berhasil");
+      handleRefreshWindow();
     } catch (error) {
       console.log(error.response.data.error);
     }
   };
 
   return {
-    handleAddCart
+    handleAddCart,
   };
 }
 
